84d3045e8106e7f0c1d1247584fb4733ad5a4d05
alopesme <alopesme@etud.u-pem.fr>
2019-12-05 15:57:29 +0100

    parser à jour

--- RETRO_ASM/src/main/java/RETRO_PROJECT/RETRO_ASM/App.java
+++ RETRO_ASM/src/main/java/RETRO_PROJECT/RETRO_ASM/App.java
@@ -3,7 +3,6 @@ package RETRO_PROJECT.RETRO_ASM;
 import java.io.IOException;
 import java.nio.file.Paths;
 
-import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.Opcodes;
 
@@ -15,19 +14,27 @@ import fr.umlv.parser.Parser;
  */
 public class App {
     public static void main( String[] args ) throws IOException {
-    	byte[] b = Parser.parser(Paths.get("../RetroTest/bin/fr/umlv/exemples/Exemple.class"));
+    	
+    	ClassVisitor visitor = new ClassVisitor(Opcodes.ASM7) {
+    		@Override
+    		public void visit(int version, int access, String name, String signature, String superName,
+    				String[] interfaces) {
+    			System.out.println("Version: " + (version - 43) + " super: " + superName);
+    			super.visit(version, access, name, signature, superName, interfaces);
+    		}
+    	};
+    	/*byte[] b = Parser.parsingFile(Paths.get("../RetroTest/bin/fr/umlv/exemples/Exemple.class"));
     	
     	ClassReader reader = new ClassReader(b);
     	ClassVisitor visitor = new ClassVisitor(Opcodes.ASM7) {
     		@Override
     		public void visit(int version, int access, String name, String signature, String superName,
     				String[] interfaces) {
-    			System.out.println("Version: " + (version - 44));
+    			System.out.println("Version: " + version + " super: " + superName);
     			super.visit(version, access, name, signature, superName, interfaces);
     		}
     	};
-    	
-    	reader.accept(visitor, ClassReader.EXPAND_FRAMES);
-        System.out.println( "Hello World!" );
+    	reader.accept(visitor, ClassReader.EXPAND_FRAMES);*/
+    	Parser.parser(Paths.get("../RetroTest/bin"), visitor);
     }
 }
--- RETRO_ASM/src/main/java/fr/umlv/parser/Parser.java
+++ RETRO_ASM/src/main/java/fr/umlv/parser/Parser.java
@@ -4,8 +4,26 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class Parser {
-	public static byte[] parser(Path path) throws IOException {
-		return Files.readAllBytes(path);
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassVisitor;
+
+public class Parser { 
+	
+	public static void parser(Path path, ClassVisitor visitor) throws IOException {
+		try(var walk = Files.walk(path)) {
+			walk.filter(p -> p.toString().endsWith(".class")).
+			map(Parser::parsingFile).map(ClassReader::new).
+			forEach(reader -> reader.accept(visitor, ClassReader.EXPAND_FRAMES));
+		}
 	}
+	
+	private static byte[] parsingFile(Path path) {
+		try {
+			return Files.readAllBytes(path);
+		} catch (IOException e) {
+			throw new AssertionError(e);
+		}
+	}
+	
+	
 }
