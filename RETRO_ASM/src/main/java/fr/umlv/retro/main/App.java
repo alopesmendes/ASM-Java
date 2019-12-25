d5fd06d6f8387e93b8dcc64607cabf4e7e0a4c6b
alopesme <alopesme@etud.u-pem.fr>
2019-12-25 11:10:05 +0100

    module-info.java crée

--- RETRO_ASM/src/main/java/fr/umlv/retro/main/App.java
+++ RETRO_ASM/src/main/java/fr/umlv/retro/main/App.java
@@ -2,21 +2,9 @@ package fr.umlv.retro.main;
 
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.Handle;
-import org.objectweb.asm.Label;
-import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
-import fr.umlv.retro.features.Concat;
-import fr.umlv.retro.features.Feature;
-import fr.umlv.retro.features.FeaturesHistory;
-import fr.umlv.retro.features.Lambdas;
-import fr.umlv.retro.features.Nestmates;
-import fr.umlv.retro.features.TryWithRessources;
-import fr.umlv.retro.observer.ObserverHistory;
 import fr.umlv.retro.observer.ObserverVisitor;
 import fr.umlv.retro.parser.Parser;
 
@@ -24,6 +12,6 @@ public class App {
     public static void main( String[] args ) throws IOException {
     	ObserverVisitor visitor = new ObserverVisitor(Opcodes.ASM7);
     	Parser.parserRead(Paths.get("../Yo/src/concat/lambda"), visitor);
-    	System.out.println(visitor.displayFeatureHistory(Lambdas.class));
+    	System.out.println(visitor);
     }
 }
---
+++ RETRO_ASM/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module retro {
+	exports fr.umlv.retro.observer;
+	exports fr.umlv.retro.options;
+	exports fr.umlv.retro.parser;
+	//exports fr.umlv.retro.features;
+	exports fr.umlv.retro.main;
+
+	requires transitive org.objectweb.asm;
+	
+	uses org.objectweb.asm.ClassReader;
+	uses org.objectweb.asm.ClassVisitor;
+	uses org.objectweb.asm.ClassWriter;
+	uses org.objectweb.asm.MethodVisitor;
+	uses org.objectweb.asm.Type;
+	
+}
