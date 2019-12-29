608e2ab9136afc17fceefc7757aef5e9cb035eac
FLambertD <flambert@etud.u-pem.fr>
2019-12-29 21:34:44 +0100

    Add JavaDoc

--- RETRO_ASM/src/main/java/fr/umlv/retro/options/OptInfo.java
+++ RETRO_ASM/src/main/java/fr/umlv/retro/options/OptInfo.java
@@ -7,13 +7,20 @@ public class OptInfo implements Option {
 
 	private OptInfo() {
 	}
-	
+	/**
+	 * Method which excecute the option -Infos
+	 */
 	@Override
 	public void execute() {
 		System.out.println("Voici les infos");
 
 	}
 	
+	/**
+	 * Method of creation of an OptInfo
+	 * @param args
+	 * @return an option instance of OptInfo
+	 */
 	public static Option create(List<String> args) {
 		Objects.requireNonNull(args);
 		return new OptInfo();
--- RETRO_ASM/src/main/java/fr/umlv/retro/options/Option.java
+++ RETRO_ASM/src/main/java/fr/umlv/retro/options/Option.java
@@ -2,5 +2,8 @@ package fr.umlv.retro.options;
 
 
 public interface Option {
+	/**
+	 * Method commun to each option which apply it
+	 * */
 	public void execute();
 }
