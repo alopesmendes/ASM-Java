5c3006cdda7c8b605a14fb2d808fa82413a4d0a2
alopesme <alopesme@etud.u-pem.fr>
2020-01-01 18:09:05 +0100

    test concat

---
+++ RETRO_ASM/src/test/java/fr/umlv/exemples/TestDetectConcat.java
@@ -0,0 +1,36 @@
+package fr.umlv.exemples;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+
+import fr.umlv.retro.features.Concat;
+import fr.umlv.retro.observer.ObserverVisitor;
+import fr.umlv.retro.parser.Parser;
+
+public class TestDetectConcat {
+	@Test
+	void testDetectConcat() {
+		Path p = Paths.get("target/test-classes/fr/umlv/exemples/TestConcat.class");
+    	ObserverVisitor ov = new ObserverVisitor();
+    	
+    	try {
+			Parser.parserRead(p, ov);
+		} catch (IOException e) {
+			throw new AssertionError(e);
+		}
+    	
+    	Set<String> set = Set.of(
+    			"CONCATENATION at TestConcat.testConcat2()V (TestConcat.java:18): pattern %1 %1 %1",
+    			"CONCATENATION at TestConcat.testConcat3()V (TestConcat.java:24): pattern %1%1",
+    			"CONCATENATION at TestConcat.testConcat()V (TestConcat.java:10): pattern uri %1 value %1.");
+    	
+    	assertEquals(set, ov.displayFeatureHistory(Concat.class).lines().collect(Collectors.toSet()));
+	}
+}
