f0f238b2bfe6799da7deb56eee5576abb41e4691
alopesme <alopesme@etud.u-pem.fr>
2020-01-01 10:00:38 +0100

    concat

--- RETRO_ASM/src/main/java/fr/umlv/retro/features/Concatenation.java
+++ RETRO_ASM/src/main/java/fr/umlv/retro/features/Concatenation.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.objectweb.asm.MethodVisitor;
@@ -12,89 +11,103 @@ import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 
 public class Concatenation implements Feature {
-	private final Consumer<MethodVisitor> begin = mv -> {
-		mv.visitTypeInsn(Opcodes.NEW, Type.getInternalName(StringBuilder.class));
-		mv.visitInsn(Opcodes.DUP);
-		mv.visitMethodInsn(Opcodes.INVOKESPECIAL, Type.getInternalName(StringBuilder.class), "<init>", "()V", false);
-	};
+	private Consumer<MethodVisitor> begin() {
+		Consumer<MethodVisitor> b =  mv -> {
+			mv.visitTypeInsn(Opcodes.NEW, Type.getInternalName(StringBuilder.class));
+			mv.visitInsn(Opcodes.DUP);
+		};
+		return b.andThen(initBuilder());
+	}
 	
-	private final Function<Type, Consumer<MethodVisitor>> append = t -> {
-		return mv -> {	 
+	private Consumer<MethodVisitor> initBuilder() {
+		return mv -> {
+			try {
+				mv.visitMethodInsn(Opcodes.INVOKESPECIAL, Type.getInternalName(StringBuilder.class),
+						"<init>", 
+						Type.getConstructorDescriptor(StringBuilder.class.getConstructor()), false);
+			} catch (NoSuchMethodException | SecurityException e) {
+				throw new AssertionError(e);
+			}
+		};
+	}
+	
+	private Consumer<MethodVisitor> append(Type t, int var) {
+		return mv -> {
 			String des = Type.getMethodDescriptor(Type.getType(StringBuilder.class), t);
-			//mv.visitVarInsn(Opcodes.ASTORE, t.getSort());
 			mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, 
-				Type.getInternalName(StringBuilder.class),
-				"append", des, false);
+					Type.getInternalName(StringBuilder.class),
+					"append", des, false);
 		};
-	};
-	
-	private final Consumer<MethodVisitor> end = mv -> {
-		mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, 
-				Type.getInternalName(StringBuilder.class),
-				"toString", "()Ljava/lang/String;", false);
+	}
+
+	private Consumer<MethodVisitor> end() {
+		return mv -> {
+			mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, 
+					Type.getInternalName(StringBuilder.class),
+					"toString", "()Ljava/lang/String;", false);
 		};
-			
+	}
+
 	private final ArrayList<String> elements;
 	private final Type[] types;
 	private int j;
-	
+
 	private Concatenation(Type[] types, List<String> elements) {
 		this.types = types;
 		this.elements = new ArrayList<>(elements);
 	}
-	
-	
+
+
 	public static Concatenation create(String description, String s) {
 		Type[] types = Type.getArgumentTypes(description);
 		List<String> list = Arrays.stream(s.split(""))
 				.collect(Collectors.toList());
 		return new Concatenation(types, list);
 	}
-	
+
 	private Consumer<MethodVisitor> ldc(String text) {
 		return  mv -> {
 			mv.visitLdcInsn(text);
 			String des = Type.getMethodDescriptor(Type.getType(StringBuilder.class), Type.getType(String.class));
 			mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, 
-				Type.getInternalName(StringBuilder.class),
-				"append", des, false);
+					Type.getInternalName(StringBuilder.class),
+					"append", des, false);
 		};
 	}
-	
-	
+
+
 	private void constantString(int k, List<Consumer<MethodVisitor>> runs) {
 		StringBuilder sb = new StringBuilder();
 		for (; j < elements.size() && !elements.get(j).equals("\u0001") && !elements.get(j).equals("\u0002"); j++) {
 			sb.append(elements.get(j));
 		}
-		k= j == elements.size() || k == (runs.size() - types.length) ? k : k-1;
+		k= j == elements.size()  ? k : k-1;
 		runs.set(k, runs.get(k).andThen(ldc(sb.toString())));
 	}
-	
+
 	private void consume(List<Consumer<MethodVisitor>> runs) {
 		int k = (runs.size() - types.length);
+		//runs.set(k, initBuilder().andThen(runs.get(k)));
 		while (j < elements.size()) {
 			Consumer<MethodVisitor> c = runs.get(k);
 			if (elements.get(j).equals("\u0001") || elements.get(j).equals("\u0002")) {
-				runs.set(k, c.andThen(append.apply(types[k - (runs.size() - types.length)])));
+				int var = k - (runs.size() - types.length);
+				runs.set(k, c.andThen(append(types[var], runs.size()-1)));
 				j++; k = Math.min(runs.size()-1, k+1);
-			} else {
-				constantString(k, runs);
-			}
+			} else { constantString(k, runs); }
 		}
-		
-	}
-	
-	@Override
-	public void rewriteFeature(int start, List<Consumer<MethodVisitor>> runs) {
-		runs.set(runs.size() - types.length, begin.andThen(runs.get(runs.size() - types.length)));
-		//IntStream.range(0, elements.size()).forEach(x -> consume(x, runs));
-		consume(runs);
-		runs.add(end);
-		
 	}
 
-	
-	
-	
+	@Override
+	public void rewriteFeature(int start, List<Consumer<MethodVisitor>> runs) {
+		runs.add(runs.size() - types.length, begin());
+
+		consume(runs);
+		runs.add(end());
+
+	}
+
+
+
+
 }
--- RETRO_ASM/src/main/java/fr/umlv/retro/features/FeatureVisitor.java
+++ RETRO_ASM/src/main/java/fr/umlv/retro/features/FeatureVisitor.java
@@ -2,15 +2,14 @@ package fr.umlv.retro.features;
 
 import java.util.ArrayList;
 import java.util.function.Consumer;
-import java.util.stream.IntStream;
 
-import org.objectweb.asm.Attribute;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Handle;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
 
 import fr.umlv.retro.observer.ObserverVisitor;
 
@@ -41,48 +40,39 @@ public class FeatureVisitor extends ClassVisitor {
 			private int start;
 			
 			@Override
-			public void visitCode() {
-				
-				ov.visitCode();
-				list.add(mv -> mv.visitCode());
-			}
-			
-			@Override
-			public void visitAnnotableParameterCount(int parameterCount, boolean visible) {
-				list.add(mv -> mv.visitAnnotableParameterCount(parameterCount, visible));
-			}
-			
-			@Override
 			public void visitMultiANewArrayInsn(String descriptor, int numDimensions) {
+				ov.visitMultiANewArrayInsn(descriptor, numDimensions);
 				list.add(mv -> visitMultiANewArrayInsn(descriptor, numDimensions));
 			}
 			
 			@Override
+			public void visitMaxs(int maxStack, int maxLocals) {
+				ov.visitMaxs(maxStack, maxLocals);
+				list.add(mv -> mv.visitMaxs(maxStack, maxLocals));
+			}
+			
+			@Override
+			public void visitLineNumber(int line, Label start) {
+				ov.visitLineNumber(line, start);
+				list.add(mv -> mv.visitLineNumber(line, start));
+			}
+			
+			@Override
 			public void visitTryCatchBlock(Label start, Label end, Label handler, String type) {
+				ov.visitTryCatchBlock(start, end, handler, type);
 				list.add(mv -> visitTryCatchBlock(start, end, handler, type));
 			}
 			
 			@Override
 			public void visitTableSwitchInsn(int min, int max, Label dflt, Label... labels) {
-				
+				ov.visitTableSwitchInsn(min, max, dflt, labels);
 				list.add(mv -> visitTableSwitchInsn(min, max, dflt, labels));
 			}
 			
 			@Override
 			public void visitLookupSwitchInsn(Label dflt, int[] keys, Label[] labels) {
+				ov.visitLookupSwitchInsn(dflt, keys, labels);
 				list.add(mv -> visitLookupSwitchInsn(dflt, keys, labels));
-			}
-			
-			@Override
-			public void visitAttribute(Attribute attribute) {
-				ov.visitAttribute(attribute);
-				list.add(mv -> mv.visitAttribute(attribute));
-			}
-			
-			@Override
-			public void visitFrame(int type, int numLocal, Object[] local, int numStack, Object[] stack) {
-				ov.visitFrame(type, numLocal, local, numStack, stack);
-				list.add(mv -> visitFrame(type, numLocal, local, numStack, stack));
 			}
 			
 			@Override
@@ -123,6 +113,13 @@ public class FeatureVisitor extends ClassVisitor {
 			}
 			
 			@Override
+			public void visitFrame(int type, int numLocal, Object[] local, int numStack, Object[] stack) {
+				start = numStack;
+				ov.visitFrame(type, numLocal, local, numStack, stack);
+				list.add(mv -> visitFrame(type, numLocal, local, numStack, stack));
+			}
+			
+			@Override
 			public void visitIntInsn(int opcode, int operand) {
 				ov.visitIntInsn(opcode, operand);
 				
@@ -142,20 +139,16 @@ public class FeatureVisitor extends ClassVisitor {
 			}
 			
 			@Override
-			public void visitLineNumber(int line, Label start) {
-				ov.visitLineNumber(line, start);
-				list.add(mv -> mv.visitLineNumber(line, start));
-			}
-			
-			@Override
-			public void visitLabel(Label label) {
-				list.add(mv -> mv.visitLabel(label));
-			}
-			
-			@Override
 			public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
 				ov.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
-				list.add(mv -> mv.visitMethodInsn(opcode, owner, name, descriptor, isInterface));				
+				Type[] t = Type.getArgumentTypes(descriptor);
+				int size = list.size()-t.length-1;
+				Consumer<MethodVisitor> c = mv -> mv.visitMethodInsn(opcode, owner, name, descriptor, isInterface);;
+				for (int i = list.size()-1; i > size; i--) {
+					c = list.remove(i).andThen(c);
+				}
+				list.add(c);
+				//list.add(mv -> mv.visitMethodInsn(opcode, owner, name, descriptor, isInterface));
 			}
 			
 			@Override
@@ -167,15 +160,11 @@ public class FeatureVisitor extends ClassVisitor {
 				//list.add(m -> m.visitInvokeDynamicInsn(name, descriptor, bootstrapMethodHandle, bootstrapMethodArguments));
 			}
 			
-			@Override
-			public void visitMaxs(int maxStack, int maxLocals) {
-				list.add(mv -> mv.visitMaxs(maxStack, maxLocals));
-			}
 			
 			@Override
 			public void visitEnd() {
 				ov.visitEnd();
-				IntStream.range(0, list.size()).forEach(i -> list.get(i).accept(mv));	
+				list.stream().forEach(c -> c.accept(mv));
 				mv.visitEnd();
 			}
 		};
